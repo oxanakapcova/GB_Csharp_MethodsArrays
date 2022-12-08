77d466ea8175817989c01d8816ab8d85ec9dea27
oxanakapcova <sakhaoks@hotmail.com>
2022-12-08 17:13:36 +0300

    created hometask43

---
+++ Sem6_ht43/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух 
+прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
+b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double b1, b2, k1, k2, x, y;
+System.Console.WriteLine("Enter b1: ");
+b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter k1: ");
+k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter b2: ");
+b2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter k2: ");
+k2 = Convert.ToDouble(Console.ReadLine());
+//k1*x+b1=k2*x+b2
+//k1*x-k2*x=b2-b1
+//(k1-k2)*x = b2-b1
+x = (b2-b1)/(k1-k2);
+y = k1*x + b1;
+System.Console.WriteLine("x = "+x+", y = "+y);
---
+++ TriangleArea/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Площадь треугольника
+По целочисленным координатам вершин треугольника (x1,y1), (x2,y2) и 
+(x3,y3) требуется вычислить его площадь.
+Входной файл INPUT.TXT содержит 6 целых чисел x1,y1,x2,y2,x3,y3 – 
+координаты вершин треугольника. Все числа не превышают 106 по 
+абсолютной величине.
+В выходной файл OUTPUT.TXT выведите точное значение площади заданного 
+треугольника.
+Примеры
+№	INPUT.TXT	   OUTPUT.TXT
+1	2 1 2 4 6 1	   6
+2	0 0 0 3 3 0	   4.5
+*/
+Console.WriteLine("Hello, Area of Triangles!");
+
+System.Console.WriteLine("Enter x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter x3: ");
+double x3 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter y3: ");
+double y3 = Convert.ToDouble(Console.ReadLine());
+double area = 0.5 * ((x2 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3));
+if (area<0)
+area*=-1;
+System.Console.WriteLine("================\n" + area);
