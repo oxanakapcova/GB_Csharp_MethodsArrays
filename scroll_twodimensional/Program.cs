a3e82ca3552fe319c4659e8b990500de2d8a4e91
oxanakapcova <sakhaoks@hotmail.com>
2022-12-21 11:58:52 +0300

    created scroll twodimensional , ht 62 from sem 9

--- scroll_twodimensional/Program.cs
+++ scroll_twodimensional/Program.cs
@@ -7,3 +7,35 @@
 10 09 08 07
 */
 Console.WriteLine("Hello, Two-dimensional arrays & scroll!");
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[size, size];
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    array[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        j++;
+    else if (i < j && i + j >= size - 1)
+        i++;
+    else if (i >= j && i + j > size - 1)
+        j--;
+    else
+        i--;
+    num++;
+}
+PrintArray(array);
